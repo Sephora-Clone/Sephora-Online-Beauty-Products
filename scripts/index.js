54644d5e75faa8474b696955cc7b17b76eedadba
rupamShaw1998 <97508688+rupamShaw1998@users.noreply.github.com>
2022-02-26 01:03:27 +0530

    api fetch of homepage contents
    by Rupam

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+const url = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=covergirl";
+
+async function getApi(url) {
+    try {
+        let res = await fetch(url);
+        let data = await res.json();
+        for(let i=0; i<34; i++)
+            data.pop();
+        console.log(data);
+        appendData(data);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+getApi(url);
+var container = document.getElementById("container");
+function appendData(data) {
+    data.forEach(function (ele) {
+        let div = document.createElement("div");
+
+        let img = document.createElement("img");
+        img.src = ele.image_link;
+
+        let h5 = document.createElement("h5");
+        h5.innerText = ele.name;
+
+        let price = document.createElement("p");
+        price.innerText = "Rs. "+Math.floor(ele.price*25);
+
+        div.append(img, h5, price);
+        container.append(div);
+})
+}
